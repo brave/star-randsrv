1fadfcbd08fc4cb79b3e4741876e9ae93721ff97
Ralph Giles <rgiles@brave.com>
2022-04-29 12:13:35 -0700

    Import the ppoprf::ffi module from sta-rs.
    We need C ffi bindings to call the randomness server implementation
    in Rust from the Go service implementation.
    
    For reproducibility we want to control the build environment and
    dependency versions of the code, so we need a Cargo.lock. It's
    simpler to maintain everything for that as part of this repository
    rather than cloning and building from a submodule.
    
    This version is based on the ppoprf-ffi branch from
    https://github.com/NullHypothesis/sta-rs

---
+++ src/lib.rs
@@ -0,0 +1,156 @@
+//! Foreign-function interface to the ppoprf randomness implementation
+//!
+//! This implements a C api so services can easily embed support.
+//!
+
+use ppoprf::ppoprf;
+
+/// Opaque struct acts as a handle to the server implementation.
+pub struct RandomnessServer {
+    inner: ppoprf::Server,
+}
+
+/// Construct a new server instance and return an opaque handle to it.
+///
+/// # Safety
+///
+/// The returned pointer is allocated by Rust and must be freed by
+/// the same allocator. The handle should be passed to the correspoding
+/// randomness_server_release() function to release the associated
+/// memory.
+// FIXME: Pass a [u8] and length for the md initialization.
+#[no_mangle]
+pub extern "C" fn randomness_server_create() -> *mut RandomnessServer {
+    let test_mds = vec!["t".into()];
+    let inner = ppoprf::Server::new(&test_mds);
+    let server = Box::new(RandomnessServer { inner });
+    Box::into_raw(server)
+}
+
+/// Release memory associated with a server instance.
+///
+/// Pass the handle returned by randomness_server_create() to this
+/// function to release the associated resources.
+///
+/// # Safety
+///
+/// The `ptr` argument must point to a valid RandomnessServer instance
+/// allocated by Rust.
+#[no_mangle]
+pub unsafe extern "C" fn randomness_server_release(ptr: *mut RandomnessServer) {
+    assert!(!ptr.is_null());
+    let server = Box::from_raw(ptr);
+    drop(server);
+}
+
+/// Evaluate the PPOPRF for the given point.
+///
+/// # Safety
+///
+/// The `ptr` argument must point to a valid RandomnessServer state
+/// struct, such as is returned by randomness_server_create().
+///
+/// The `input` and `output` arguments must point to accessible areas
+/// of memory with
+/// the correct amount of space available.
+#[no_mangle]
+pub unsafe extern "C" fn randomness_server_eval(
+    ptr: *const RandomnessServer,
+    input: *const u8,
+    md_index: usize,
+    verifiable: bool,
+    output: *mut u8,
+) {
+    // Verify arguments.
+    assert!(!ptr.is_null());
+    assert!(!input.is_null());
+    assert!(!output.is_null());
+
+    // Convert our *const argument to a &ppoprf::Server without taking ownership.
+    let server = &(*ptr).inner;
+    // Wrap the provided compressed Ristretto point in the expected type.
+    // Unfortunately from_slice() copies the data here.
+    let point = {
+        let bytes = std::slice::from_raw_parts(input, ppoprf::COMPRESSED_POINT_LEN);
+        ppoprf::CompressedRistretto::from_slice(bytes)
+    };
+    // Evaluate the requested point.
+    let result = server.eval(&point, md_index, verifiable);
+    // Copy the resulting point into the output buffer.
+    std::ptr::copy_nonoverlapping(
+        result.output.as_bytes().as_ptr(),
+        output,
+        ppoprf::COMPRESSED_POINT_LEN,
+    );
+}
+
+/// Puncture the given md value from the PPOPRF.
+///
+/// # Safety
+///
+/// The `ptr` argument must point to a valid RandomnessServer state
+/// struct, such as is returned by randomness_server_create().
+#[no_mangle]
+pub unsafe extern "C" fn randomness_server_puncture(ptr: *mut RandomnessServer, md: u8) {
+    // Convert our *const to a &ppoprf::Server without taking ownership.
+    assert!(!ptr.is_null());
+    let server = &mut (*ptr).inner;
+
+    // The ffi signature takes a u8 by value, but the underlying
+    // api wants a slice to allow more than 8 bits of metadata tag.
+    let md_vec = vec![md];
+    // Call correct function.
+    server.puncture(&md_vec);
+}
+
+#[cfg(test)]
+mod tests {
+    //! Unit tests for the ppoprf foreign-function interface
+    //!
+    //! This tests the C-compatible api from Rust for convenience.
+    //! Testing it from other langauges is also recommended!
+
+    use crate::*;
+    use curve25519_dalek::ristretto::CompressedRistretto;
+
+    #[test]
+    /// Verify creation/release of the opaque server handle.
+    fn unused_instance() {
+        let server = randomness_server_create();
+        assert!(!server.is_null());
+        unsafe {
+            randomness_server_release(server);
+        }
+    }
+
+    #[test]
+    /// One evaluation call to the ppoprf.
+    fn simple_eval() {
+        let server = randomness_server_create();
+        assert!(!server.is_null());
+
+        // Evaluate a test point.
+        let point = CompressedRistretto::default();
+        let mut result = Vec::with_capacity(ppoprf::COMPRESSED_POINT_LEN);
+        unsafe {
+            randomness_server_eval(
+                server,
+                point.as_bytes().as_ptr(),
+                0,
+                false,
+                result.as_mut_ptr(),
+            );
+            // FIXME: verify result!
+            randomness_server_release(server);
+        }
+    }
+
+    #[test]
+    /// Verify serialization of internal types.
+    fn serialization() {
+        let point = CompressedRistretto::default();
+        println!("{:?}", &point);
+
+        // ppoprf::Evaluation doesn't implement Debug.
+    }
+}
