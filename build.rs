8d7accd7890cfb59ed43c43f77a751a863b1673a
Ralph Giles <rgiles@brave.com>
2022-11-15 11:41:56 -0800

    Remove unnecessary borrow
    `cbindgen::generate` takes an `AsRef<Path>` which is implemented
    for `String` so it's not necessary to pass a borrow. Slightly
    confusing in general, but here we don't use the value again,
    so if it looks like bindgen has taken ownership, that's ok?
    
    Addresses a clippy lint with rust 1.67 nightly.

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::generate(&crate_dir)
+    cbindgen::generate(crate_dir)
         .expect("Unable to generate C bindings")
         .write_to_file("include/ppoprf.h");
 }
