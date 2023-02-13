acdb3fb10ebb619bd30fffa99d7e0e52975d70bb
Ralph Giles <rgiles@brave.com>
2023-02-13 15:26:28 -0800

    Remove old nitriding implementation
    Instead of linking over ffi from the go nitriding framework,
    build it as a separate proxy daemon which runs inside the
    the same container as star-randsrv.

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+nitriding \
+	-acme \
+	-fqdn "nitro.nymity.ch" \
+	-appurl "https://github.com/brave/star-randsrv" \
+	-appwebsrv "http://127.0.0.1:8080" \
+	-extport 8443 \
+	-intport 8081 &
+echo "[sh] Started nitriding as reverse proxy."
+
+sleep 1
+
+star-randsrv
+echo "[sh] Started star-randsrv."
