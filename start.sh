f2701edd5f181d1f3c77d0be1386d1a32947fb65
Darnell Andries <dandries@brave.com>
2024-07-22 15:00:58 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 nitriding \
-  -fqdn "star-randsrv.bsg.brave.com" \
+  -fqdn "star-randsrv.bsg.brave.software" \
+  -fqdn-leader "star-randsrv-leader:9443" \
   -appurl "https://github.com/brave/star-randsrv" \
   -appwebsrv "http://127.0.0.1:8080" \
-  -prometheus-port 9090 \
   -vsock-ext \
   -disable-keep-alives \
-  -prometheus-namespace "nitriding" \
-  -extport 443 \
+  -host-ip-provider-port 6161 \
+  -ext-priv-port 9443 \
   -intport 8081 &
 echo "[sh] Started nitriding as reverse proxy."
 
@@ -22,5 +22,8 @@ star-randsrv \
   --instance-name slow \
   --epoch-duration "1mon" \
   --epoch-base-time 2023-05-01T00:00:00Z \
-  --increase-nofile-limit
+  --increase-nofile-limit \
+  --enclave-key-sync \
+  --nitriding-internal-port 8081
+
 echo "[sh] Started star-randsrv."
