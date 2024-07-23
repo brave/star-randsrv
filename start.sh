b835255396af385de6d48c492e47c13b79cec299
Darnell Andries <dandries@brave.com>
2024-07-22 19:12:59 -0700

    Start nitriding and star-randsrv as background processes

--- start.sh
+++ start.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-nitriding \
-  -fqdn "star-randsrv.bsg.brave.software" \
-  -fqdn-leader "leader-internal-sync-service.star-randsrv-dev.svc.cluster.local:9443" \
-  -appurl "https://github.com/brave/star-randsrv" \
-  -appwebsrv "http://127.0.0.1:8080" \
-  -vsock-ext \
-  -disable-keep-alives \
-  -host-ip-provider-port 6161 \
-  -ext-priv-port 9443 \
-  -intport 8081 &
-echo "[sh] Started nitriding as reverse proxy."
-
-sleep 1
-
 star-randsrv \
   --instance-name typical \
   --epoch-duration "1w" \
@@ -24,6 +10,26 @@ star-randsrv \
   --epoch-base-time 2023-05-01T00:00:00Z \
   --increase-nofile-limit \
   --enclave-key-sync \
-  --nitriding-internal-port 8081
-
+  --nitriding-internal-port 8081 &
+star_randsrv_pid=$!
 echo "[sh] Started star-randsrv."
+
+sleep 5
+
+nitriding \
+  -fqdn "star-randsrv.bsg.brave.software" \
+  -fqdn-leader "leader-internal-service.star-randsrv-dev.svc.cluster.local:9443" \
+  -appurl "https://github.com/brave/star-randsrv" \
+  -appwebsrv "http://127.0.0.1:8080" \
+  -vsock-ext \
+  -disable-keep-alives \
+  -host-ip-provider-port 6161 \
+  -ext-priv-port 9443 \
+  -intport 8081 &
+nitriding_pid=$!
+echo "[sh] Started nitriding as reverse proxy."
+
+while true; do
+  ([ -d "/proc/$nitriding_pid" ] && [ -d "/proc/$star_randsrv_pid" ]) || exit 1
+  sleep 5
+done
